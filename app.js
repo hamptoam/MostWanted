cad13522c72a484864b32b9879da372bbdbcbf0c
hamptoam <ameliabedelia94@gmail.com>
2019-10-07 15:56:16 -0500

    Final commit before resubmission, attempted recursion.

--- app.js
+++ app.js
@@ -19,23 +19,23 @@ function app(people) {
 }
 
 function mainMenu(foundPerson, people) {
-var person = foundPerson;
+  var person = foundPerson;
   if (!person) {
     alert("Could not find that individual.");
     return app(people);
   }
 
-  var displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
+  var displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'spouse', or 'descendants'? Type the option you want or 'restart' or 'quit'");
 
   switch (displayOption) {
     case "info":
       displayPerson(person, people);
       break;
-    case "Spouse":
-        findSpouse(person, people);
+    case "spouse":
+      findSpouse(person, people);
       break;
-    case "Children":
-        findChildren(person, people);
+    case "descendants":
+      findChildren(person, people);
       break;
     case "restart":
       app(people); // restart
@@ -52,7 +52,6 @@ var person = foundPerson;
 function searchByName(people) {
   var firstName = promptFor("What is the person's first name?", chars);
   var lastName = promptFor("What is the person's last name?", chars);
-
 
   var foundPerson = people.filter(function (person, data) {
     if (person.firstName === firstName && person.lastName === lastName) {
@@ -71,13 +70,12 @@ function searchByTraits(people) {
   knowEyeColor = promptFor("Do you know the person's eyecolor? yes or no", yesNo);
   knowWeight = promptFor("Do you know the person's weight? yes or no", yesNo);
   knowOccupation = promptFor("Do you know the person's occupation? yes or no", yesNo);
- 
+
   if (knowGender == "yes") {
     gender = promptFor("What is their gender?", chars).toLowerCase();
 
     personGender = people.filter(function (person) {
       if (person.gender == gender) {
-        console.log(person.firstName);
         return true;
       } else {
         return false;
@@ -86,7 +84,6 @@ function searchByTraits(people) {
     var people = personGender;
     if (people.length == 1) {
       var foundPerson = personGender[0];
-      console.log(foundPerson);
       displayPerson(foundPerson);
       return foundPerson;
     }
@@ -107,8 +104,8 @@ function searchByTraits(people) {
       var foundPerson = people[0];
       return foundPerson;
     }
-  }   else {
-  return false;
+  } else {
+    return false;
   }
   if (knowEyeColor == "yes") {
     color = promptFor("What is the person's eyecolor?", chars).toLowerCase();
@@ -128,8 +125,8 @@ function searchByTraits(people) {
   }
   if (knowWeight == "yes") {
     weight = promptFor("What is the person's weight?", chars).toLowerCase();
-    personWeight = people.filter(function(person) {
-      if (person.weight == weight){
+    personWeight = people.filter(function (person) {
+      if (person.weight == weight) {
         return true;
       } else {
         return false;
@@ -166,94 +163,91 @@ function getAge(person) {
   var age = today.getFullYear() - birthDate.getFullYear();
   var m = today.getMonth() - birthDate.getMonth();
   if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-      age--;
+    age--;
   }
   return age;
 }
 
-//var person = searchByTraits.foundPerson;
-
 function findSpouse(foundPerson, people) {
- // "use strict" ; person = searchByTraits(foundPerson);
+  // "use strict" ; person = searchByTraits(foundPerson);
   var spouseId = foundPerson.currentSpouse;
-  var partner = people.filter(function(person){
-    for(var i = 0; i < people.length; i++){
-      if (person.id === spouseId){
-       alert("Spouse is " + " " + person.firstName + " " + person.lastName);
-      return partner;
-        } else {
-      return false;
+  var partner = people.filter(function (person) {
+    for (var i = 0; i < people.length; i++) {
+      if (person.id === spouseId) {
+        alert("Spouse is " + " " + person.firstName + " " + person.lastName);
+        return partner;
+      } 
+      if(spouseId == null)
+      {
+        alert(foundPerson.firstName + " " + "has no spouse");
+        return mainMenu(foundPerson);
+      }
     }
-  }});
+  });
 }
- 
-function findChildren(foundPerson, people){
+
+function findChildren(foundPerson, people) {
   var foundChildren = [];
   var parent = foundPerson;
-  for(var i = 0; i <  people.length; i++){
-      foundChildren = people.filter(function(childPerson) {
+  for (var i = 0; i < people.length; i++) {
+    foundChildren = people.filter(function (childPerson) {
       if (parent.id === childPerson.parents[i]) {
-        console.log(childPerson);
         alert("Children: " + " " + childPerson.firstName + " " + childPerson.lastName);
         foundChildren.push(childPerson);
-        var child = childPerson;
-        getDescendants(child);
         return true;
       } else {
         alert("Person has no children");
         return false;
       }
-     });
+    });
+  }
+}
+  /*function getDescendants(foundPerson, people)
+  {
+    for (var i = 0; i < foundChildren.length; i++) {
+      return findChildren(childPerson, foundChildren[i]);
     }
-    var child = childPerson;
-    return foundChildren[i];
-}
-function getDescendants (people, children){
- // var foundChildren[];
-   for( let i = 0; i < foundChildren.length; i++){
-       return findChildren(people, foundChildren.parents[i].id);
-   }
-}
-  var nameString = "";
-    alert(foundGrandChildren(function(el){
-      nameString += el.firstName + " " + el.lastName + "\n";
-      alert("Grandchild");
-    }));
+    if (childPerson == null) {
+      alert("Person has no grandchildren");
+    } else {
+      alert("GrandChild: " + childPerson.firstName + " " + childPerson.lastName);
+    }
+  } */
 
-function displayPeople(people) {
-  alert(people.map(function (person) {
-    return person.firstName + " " + person.lastName;
-  }).join("\n"));
-}
+  function displayPeople(people) {
+    alert(people.map(function (person) {
+      return person.firstName + " " + person.lastName;
+    }).join("\n"));
+  }
 
-function displayPerson(person) {
-  var personInfo = "First Name: " + person.firstName + "\n";
-  personInfo += "Last Name: " + person.lastName + "\n";
-  // TODO: finish getting the rest of the information to display
-  personInfo += "Height:" + person.height + "\n";
-  personInfo += "Weight" + person.weight + "\n";
-  personInfo += "Date of Birth:" + person.dob + "\n";
-  personInfo += "Occupation:" + person.occupation + "\n";
-  personInfo += "Eye Color:" + person.eyeColor + "\n";
-  personInfo += "Spouse: " + person.currentSpouse + "\n";
-  alert(personInfo);
-  return (personInfo);
-}
+  function displayPerson(person) {
+    var personInfo = "First Name: " + person.firstName + "\n";
+    personInfo += "Last Name: " + person.lastName + "\n";
+    // TODO: finish getting the rest of the information to display
+    personInfo += "Height:" + person.height + "\n";
+    personInfo += "Weight" + person.weight + "\n";
+    personInfo += "Date of Birth:" + person.dob + "\n";
+    personInfo += "Occupation:" + person.occupation + "\n";
+    personInfo += "Eye Color:" + person.eyeColor + "\n";
+    personInfo += "Spouse: " + person.currentSpouse + "\n";
+    alert(personInfo);
+    return (personInfo);
+  }
 
-// function that prompts and validates user input
-function promptFor(question, valid) {
-  do {
-    var response = prompt(question).trim();
-  } while (!response || !valid(response));
-  return response;
-}
+  // function that prompts and validates user input
+  function promptFor(question, valid) {
+    do {
+      var response = prompt(question).trim();
+    } while (!response || !valid(response));
+    return response;
+  }
 
-// helper function to pass into promptFor to validate yes/no answers
-function yesNo(input) {
-  return input.toLowerCase() == "yes" || input.toLowerCase() == "no";
-}
+  // helper function to pass into promptFor to validate yes/no answers
+  function yesNo(input) {
+    return input.toLowerCase() == "yes" || input.toLowerCase() == "no";
+  }
 
-// helper function to pass in as default promptFor validation
-function chars(input) {
-  return true; // default validation only
-}
+  // helper function to pass in as default promptFor validation
+  function chars(input) {
+    return true; // default validation only
+  }
