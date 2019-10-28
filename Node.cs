c6929ae85fbbc18fe94c8725ec712e4ba3d0f7e5
hamptoam <ameliabedelia94@gmail.com>
2019-10-28 16:40:57 -0500

    Reworked Add and Search methods, resubmitting.

---
+++ Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class Node
+    {
+        public int value;
+        public Node left { get; set; }
+        public Node right { get; set; }
+
+        public Node(int value)
+        {
+            this.value = value;
+        }
+  
+    }
+}
+    
+
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySeachTree tree = new BinarySeachTree();
+            Node newNode = new Node(30);
+            tree.Add(newNode);
+            Node secondNode = new Node(44);
+            newNode = secondNode;
+            tree.Add(secondNode);
+            Node thirdNode = new Node(38);
+            newNode = thirdNode;
+            tree.Add(thirdNode);
+            Node fourthNode = new Node(55);
+            newNode = fourthNode;
+            tree.Add(fourthNode);
+            Console.WriteLine(tree);
+            Console.ReadLine();
+        }
+    }
+}
